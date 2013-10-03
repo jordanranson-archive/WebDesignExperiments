454197f517720f0f02b1ca6b4177f6a934d4175b
Jordan Ranson <jordan@jordanranson.com>
2013-10-03 05:33:04 -0600

    Added baller canvas background

--- js/site.js
+++ js/site.js
@@ -3,7 +3,12 @@ var arrLen,
 	width,
 	height,
 	canvas,
-	context;
+	context,
+	pattern,
+	triangle = {},
+	mousex = 0,
+	mousey = 0,
+	color = [ '#0c0c0c', '#0f0f0f', '#121212', '#151515' ];
 
 // polyfill
 window.requestAnimFrame = (function(){
@@ -16,7 +21,7 @@ window.requestAnimFrame = (function(){
 })();
 
 window.onload = function() {
-	width = window.innerWidth/64<<0+1;
+	width = window.innerWidth/64<<0+2;
 	height = window.innerHeight/64<<0+1;
 	arrLen = (width*height)*4;
 	points = new Float32Array( arrLen );
@@ -25,6 +30,9 @@ window.onload = function() {
 	canvas.height = window.innerHeight;
 	context = canvas.getContext( '2d' );
 
+	triangle.side = 64;
+	triangle.h = triangle.side * (Math.sqrt(3)/2);
+
 	// create triangle points
 	var row, col, index;
 	for( var i = arrLen-1; i >= 3; i-=4 ) {
@@ -32,13 +40,36 @@ window.onload = function() {
 		col = index%width;
 		row = index/width << 0;
 
-		points[i-3] = col*64;
-		points[i-2] = row*64;
+		points[i-3] = col*31.5;
+		points[i-2] = row*54.5;
 		points[i-1] = Math.random()*4 << 0;
 		points[i] = 0;
 	}
 
-	console.log( points );
+	// create bg pattern
+	var bb = document.createElement( 'canvas' );
+		bb.width = 5;
+		bb.height = 5;
+	var ctx = bb.getContext( '2d' );
+		ctx.fillStyle = color[3];
+		ctx.fillRect(0,0,5,5);
+		ctx.fillStyle = '#000';
+		ctx.fillRect(0,0,1,1);
+		ctx.fillRect(1,1,1,1);
+		ctx.fillRect(2,2,1,1);
+		ctx.fillRect(3,3,1,1);
+		ctx.fillRect(4,4,1,1);
+	pattern = context.createPattern( bb, 'repeat' );
+
+	this.addEventListener( 'mousemove', function( evt ) {
+		mousex = evt.clientX;
+		mousey = evt.clientY;
+	});
+
+	this.addEventListener( 'resize', function( evt ) { 
+		canvas.width = this.innerWidth;
+		canvas.height = this.innerHeight;
+	});
 
 	// start animating that shit
 	(function animloop() {
@@ -52,17 +83,56 @@ function render() {
 	context.clearRect( 0, 0, canvas.width, canvas.height );
 	context.save();
 
-	context.fillStyle = '#fff';
 
-	var x, y, a, b;
+	var x, y, a, b, dir, index, row, col, dist;
 	for( var i = arrLen-1; i >= 3; i-=4 ) {
+		index = ((i+1)/4)-1;
+		row = index/width << 0;
+		col = index%width;
+		dir = row % 2 === 0 ? 
+			  col % 2 === 0 ? 0 : 180 :
+			  col % 2 === 0 ? 180 : 0;
+
 		x = points[i-3];
 		y = points[i-2];
 		a = points[i-1];
 		b = points[i];
 
-		context.fillRect( x-1, y-1, 2, 2 );
+		dist = distanceTo( x, y, canvas.width/2, canvas.height/2 );
+		dist = Math.min( Math.max( dist, 0 ), canvas.width*0.46 );
+		dist /= canvas.width*0.46;
+
+		context.fillStyle = color[a];
+		context.globalAlpha = 1-dist;
+		drawTriangle( x, y, dir );
 	}
 
 	context.restore();
+}
+
+function drawTriangle( x, y, deg ) {
+    context.save();
+
+    context.translate( x, y );
+	context.rotate( rad( deg ) );
+
+    context.beginPath();
+        
+    context.moveTo( 0, -triangle.h/2 );
+    context.lineTo( -triangle.side/2, triangle.h/2 );
+    context.lineTo( triangle.side/2, triangle.h/2 );
+    context.lineTo( 0, -triangle.h/2 );
+        
+    context.closePath();
+    context.fill();
+
+    context.restore();
+}
+
+function distanceTo( x1, y1, x2, y2 ) {
+	return Math.sqrt( (x1 -= x2) * x1 + (y1 -= y2) * y1 );
+}
+
+function rad( deg ) {
+	return deg * ( Math.PI / 180 );
 }
